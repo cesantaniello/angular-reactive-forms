af1d6d36d838b68d8774c9706792e205f93d9c3e
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-02 18:06:19 +0200

    Create basic register form

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -14,6 +14,9 @@ export class FormUtils {
 
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`;
+
+        case 'email':
+          return 'El valor debe de ser un email válido';
       }
     }
 
