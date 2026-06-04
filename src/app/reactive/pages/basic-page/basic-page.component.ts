2aca9c0bab8e105abc590f586628e8eeceb2de44
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-04 17:05:30 +0200

    Remove commented code from basic page component

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -2,7 +2,6 @@ import { JsonPipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import {
   FormBuilder,
-  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -25,40 +24,6 @@ export class BasicPageComponent {
   });
 
   savedProducts = signal<{ name: string; price: number; inStorage: number }[]>([]);
-
-  // myForm2 = new FormGroup({
-  //   name: new FormControl('', [], []),
-  //   price: new FormControl(0),
-  //   inStorage: new FormControl(0),
-  // });
-
-  // isValidField(fieldName: string): boolean | null {
-  //   return (
-  //     this.myForm.controls[fieldName].errors &&
-  //     this.myForm.controls[fieldName].touched
-  //   );
-  // }
-
-  // getFieldError(fieldName: string): string | null {
-  //   if (!this.myForm.controls[fieldName]) return null;
-
-  //   const errors = this.myForm.controls[fieldName].errors ?? {};
-
-  //   for (const key of Object.keys(errors)) {
-  //     switch (key) {
-  //       case 'required':
-  //         return 'Este campo es requerido';
-
-  //       case 'minlength':
-  //         return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
-
-  //       case 'min':
-  //         return `Valor mínimo de ${errors['min'].min}`;
-  //     }
-  //   }
-
-  //   return null;
-  // }
 
   onDelete(index: number) {
     this.savedProducts.update(products => products.filter((_, i) => i !== index));
